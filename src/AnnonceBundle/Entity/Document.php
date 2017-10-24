dc40f0b1b2f2cb47ae6fe9cd2c0913333194f406
audrybab <audry.babela@senprog.com>
2017-10-24 17:00:24 +0100

    Ajout entité document et ressources bootstrap

---
+++ src/AnnonceBundle/Entity/Document.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace AnnonceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * Document
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="document")
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Entity(repositoryClass="AnnonceBundle\Repository\DocumentRepository")
+ */
+class Document
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    public $name;
+
+    /**
+     * @Assert\File(maxSize="6000000")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    public $path;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    public $dateajout;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AnnonceBundle\Entity\Annonce", inversedBy="documents")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $annonce;
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->path)) {
+            // store the old name to delete after the update
+            $this->temp = $this->path;
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    public function getUploadPicture()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/';
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        // la propriété « file » peut être vide si le champ n'est pas requis
+        if (null === $this->file) {
+            return;
+        }
+
+        if ($this->path != $this->file->getClientOriginalName()) {
+            $this->path = $this->file->getClientOriginalName();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload($nouveauNom)
+    {
+        // la propriété « file » peut être vide si le champ n'est pas requis
+        if (null === $this->file) {
+            return;
+        }
+
+        // la méthode « move » prend comme arguments le répertoire cible et
+        // le nom de fichier cible où le fichier doit être déplacé
+        if (!file_exists($this->getUploadRootDir())) {
+            mkdir($this->getUploadRootDir(), 0775, true);
+        }
+
+        $this->file->move(
+            $this->getUploadDir(), $nouveauNom
+        );
+        $this->file = null;
+    }
+
+    /*
+     * Genere de façon aléatoire un nom de fichier
+     */
+    public function genereNom($longueur){
+    //Under the string $Caracteres you write all the characters you want to be used to randomly generate the code.
+        $Caracteres = 'ABCDEFGHIJKLMOPQRSTUVXWYZ0123456789';
+        $QuantidadeCaracteres = strlen($Caracteres);
+        $QuantidadeCaracteres--;
+
+        $Hash=NULL;
+        for($x=1;$x<=$longueur;$x++){
+            $Posicao = rand(0,$QuantidadeCaracteres);
+            $Hash .= substr($Caracteres,$Posicao,1);
+        }
+
+        return $Hash;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Document
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     *
+     * @return Document
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set dateajout
+     *
+     * @param \DateTime $dateajout
+     *
+     * @return Document
+     */
+    public function setDateajout($dateajout)
+    {
+        $this->dateajout = $dateajout;
+
+        return $this;
+    }
+
+    /**
+     * Get dateajout
+     *
+     * @return \DateTime
+     */
+    public function getDateajout()
+    {
+        return $this->dateajout;
+    }
+
+
+    /**
+     * Set annonce
+     *
+     * @param \AnnonceBundle\Entity\Annonce $annonce
+     *
+     * @return Document
+     */
+    public function setAnnonce(\AnnonceBundle\Entity\Annonce $annonce = null)
+    {
+        $this->annonce = $annonce;
+
+        return $this;
+    }
+
+    /**
+     * Get annonce
+     *
+     * @return \AnnonceBundle\Entity\Annonce
+     */
+    public function getAnnonce()
+    {
+        return $this->annonce;
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
